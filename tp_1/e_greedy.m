d362ee0bc03b2c11e1d96ceead2d17c76636f457
phlf <ph.lefur@gmail.com>
2015-11-24 17:12:53 +0100

    Improvements to Q learning and function plots

--- tp_1/e_greedy.m
+++ tp_1/e_greedy.m
@@ -1,4 +1,4 @@
-%{
+%{ 
 Copyright (C) 2015 Le Fur Pierre-Henri
  
  This program is free software; you can redistribute it and/or modify it
@@ -14,7 +14,7 @@ Copyright (C) 2015 Le Fur Pierre-Henri
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- -*- texinfo -*-
+ -*- texinfo -*- 
  @deftypefn {Function File} {@var{retval} =} e-greedy (@var{input1}, @var{input2})
 
  @seealso{}
@@ -22,19 +22,19 @@ Copyright (C) 2015 Le Fur Pierre-Henri
 
  Author: Le Fur Pierre-Henri <lpierre-henri@hpph>
  Created: 2015-11-23
-%}
+ %}
 
-function [action] = e_greedy (state, iter, iter_max, Q)
+function [epsilon, action] = e_greedy (state, iter, iter_max, Q)
 
-
-epsilon= 1-(iter/iter_max);
+tau=(iter_max^2)/10;
+epsilon= exp(-(iter^2/tau));
 r=rand(1);
 
 
 if(0<=r)&&(r<=epsilon)
-    action = randi(size(Q, 2)); %On choisit en fonction de l'tat actuel parmi les tats suivants possibles.
+  action = randi(size(Q, 2)); %On choisit en fonction de l'etat actuel parmi les etats suivants possibles.
 elseif(epsilon<r)&&(r<=1)
-    [~, action] = max(Q(state,:));
+  [dummy, action] = max(Q(state,:));
 end
 
 end
--- tp_1/main_Qlearning.m
+++ tp_1/main_Qlearning.m
@@ -2,13 +2,14 @@ states = zeros(1, 10);
 policy = ones (1,16);
 value = zeros (1,16);
 
-%Comprendre ce qu'il se passe, si il faut apprendre en m^eme temps etc
+% Comprendre ce qu'il se passe, si il faut apprendre en m^eme temps etc
 
-%Q-learning
+% Q-learning
 Q = rand(16,4);
 E = rand(16);
+iter_max=1000;
 
-%Initialisation
+% Initialisation
 n = 0.1;      % Poids des recompenses avenir
 gamma = 0.9;  % Recompense prochaine steps
 
@@ -17,24 +18,39 @@ s_inter  = 0;
 s = ceil(rand*16);
 
 
-for i = 1:1000
-    a = e_greedy(s,i,1000,Q);
-    [s_inter, r] = go(s,a);
+for k = 1:iter_max
+    [e, a] = e_greedy(s,k,iter_max,Q);
+    [s_inter, r] = go(s,a);  
+    
     Q(s, a) = Q(s, a) +  n*(r + gamma*max(Q(s_inter, :)) - Q(s, a));
+    
+    % Pour observer la convergence.
+    Q_plot(k)= abs(n*(r + gamma*max(Q(s_inter, :)) - Q(s, a)));
+    
     s = s_inter;
+    
+    epsilon(k)=e;
+    iter(k)=k;
+    
+    
 end
 
+plot(iter,epsilon,iter,Q_plot,'r');
+hold on;
 
 
-%Initialise l'etat initiale de toto
+% Initialise l'etat initiale de toto
 states(1) = ceil(rand*16);
 
-%Selon l'etat precedent on calcul l etat suivant
+% A partir d'ici on veut vérifier si l'apprentissage a bien fonctionné
+
+% Selon l'etat precedent on calcul l etat suivant
 for u = 2:10
-    a = e_greedy(states(u-1), u, 10,Q); % e_greedy trop fort pour ce cas à retravailler
+    [e, a] = e_greedy(states(u-1), u, 10,Q);
     s_inter = go(states(u-1), a);
     states(u) = s_inter(1);
 end
 
-%Generation image toto
+% Generation image toto
+figure();
 walkshow(states','toto.png');
