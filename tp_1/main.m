c8b50e7857f035460d57c6a6f152bdd0b6fed39a
Vincent Germain <vincent.germain93@gmail.com>
2015-11-29 18:57:25 +0100

    Replace # to %

--- tp_1/main.m
+++ tp_1/main.m
@@ -2,45 +2,45 @@ states = zeros(1, 10);
 policy = ones (1,16);
 value = zeros (1,16);
 
-#Position possible quand on marche 2 3 4 5 8 9 13 14 
-rew = [ 0,-1,0,-1;  # position 1 non-voulue, les actions 0 lui permettent de revenir dans une position normale
-        -1,0,-1,0;  # position 2 on donne -1 quand le mouvement est impossible ou si il retourne en arrière.
-        0,-1,-1,-1;  # //
-        -1,-1,1,-1; # Position 4 intermédiaire,on donne une récompense à l'action qui le fait avancer 
+%Position possible quand on marche 2 3 4 5 8 9 13 14 
+rew = [ 0,-1,0,-1;  % position 1 non-voulue, les actions 0 lui permettent de revenir dans une position normale
+        -1,0,-1,0;  % position 2 on donne -1 quand le mouvement est impossible ou si il retourne en arrière.
+        0,-1,-1,-1;  % //
+        -1,-1,1,-1; % Position 4 intermédiaire,on donne une récompense à l'action qui le fait avancer 
         
-        -1,0,-1,0;  # CF position 2
-        0,-1,0,-1;  # CF position 1
-        0,-1,0,-1;  # CF position 1
-        -1,0,-1,-1;  # CF position 2
+        -1,0,-1,0;  % CF position 2
+        0,-1,0,-1;  % CF position 1
+        0,-1,0,-1;  % CF position 1
+        -1,0,-1,-1;  % CF position 2
         
         -1,-1,0,-1;
         -1,-1,0,-1;
-        -1,-1,-1,-1;  # Position 11, ne marche pas si g petit sinon 0,-1,0,-1;
+        0,-1,0,-1;%-1,-1,-1,-1;  % Position 11, ne marche pas si g petit sinon 0,-1,0,-1;
         -1,0,-1,0;
         
-        1,-1,-1,-1; # CF position 4
+        1,-1,-1,-1; % CF position 4
         -1,-1,-1,0; 
-        -1,-1,-1,-1;  # CF position 11 sinon   -1,0,-1,0;
-        -1,0,-1,0]; # CF position 1
+        -1,0,-1,0;%-1,-1,-1,-1;  % CF position 11 sinon   -1,0,-1,0;
+        -1,0,-1,0]; % CF position 1
 
         
-# Facteur d'actualisation
-g = 0.5;
+% Facteur d'actualisation
+g = 0.9;
 
 trans = [ 2,4,5,13;
           1,3,6,14;
           4,2,7,15;
-          3,1,8,16;#4
+          3,1,8,16;%4
           
           6,8,1,9;
           5,7,2,10;
           8,6,3,11;
-          7,5,4,12;#8
+          7,5,4,12;%8
           
           10,12,13,5;          
           9,11,14,6;          
           12,10,15,7;
-          11,9,16,8;#12
+          11,9,16,8;%12
           
           14,16,9,1;
           13,15,10,2;                 
